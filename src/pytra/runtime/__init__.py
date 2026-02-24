0d44c741ed11581815a61fc88f577539f958b013
yaneurao <yaneurao@gmail.com>
2026-02-25 05:45:18 +0900

    chore: restore sample18 js/ts sample runtime imports and doc progress

---
+++ src/pytra/runtime/__init__.py
@@ -0,0 +1,5 @@
+"""Compatibility shim for legacy `pytra.runtime` imports."""
+
+from . import gif, png
+
+__all__ = ["gif", "png"]
---
+++ src/pytra/runtime/png.py
@@ -0,0 +1,3 @@
+from ..utils.png import write_rgb_png
+
+__all__ = ["write_rgb_png"]
