a7897b57bb69d67fd56384c8dfa4d91de0178aa7
yaneurao <yaneurao@gmail.com>
2026-02-27 01:16:02 +0900

    chore(sample): regenerate 06_julia_parameter_sweep.cpp

--- sample/cpp/06_julia_parameter_sweep.cpp
+++ sample/cpp/06_julia_parameter_sweep.cpp
@@ -4,17 +4,13 @@
 #include "pytra/std/time.h"
 #include "pytra/utils/gif.h"
 
-
-
-
-
 bytes julia_palette() {
     bytearray palette = bytearray(256 * 3);
     palette[0] = 0;
     palette[1] = 0;
     palette[2] = 0;
     for (int64 i = 1; i < 256; ++i) {
-        float64 t = (py_to_float64(i - 1)) / 254.0;
+        float64 t = py_div((py_to<float64>(i - 1)), 254.0);
         int64 r = int64(255.0 * 9.0 * (1.0 - t) * t * t * t);
         int64 g = int64(255.0 * 15.0 * (1.0 - t) * (1.0 - t) * t * t);
         int64 b = int64(255.0 * 8.5 * (1.0 - t) * (1.0 - t) * (1.0 - t) * t);
@@ -29,9 +25,9 @@ bytes render_frame(int64 width, int64 height, float64 cr, float64 ci, int64 max_
     bytearray frame = bytearray(width * height);
     for (int64 y = 0; y < height; ++y) {
         int64 row_base = y * width;
-        float64 zy0 = -1.2 + 2.4 * (py_to_float64(y) / (py_to_float64(height - 1)));
+        float64 zy0 = -1.2 + 2.4 * (py_div(py_to<float64>(y), (py_to<float64>(height - 1))));
         for (int64 x = 0; x < width; ++x) {
-            float64 zx = -1.8 + 3.6 * (py_to_float64(x) / (py_to_float64(width - 1)));
+            float64 zx = -1.8 + 3.6 * (py_div(py_to<float64>(x), (py_to<float64>(width - 1))));
             float64 zy = zy0;
             int64 i = 0;
             while (i < max_iter) {
@@ -69,14 +65,14 @@ void run_06_julia_parameter_sweep() {
     int64 start_offset = 20;
     int64 phase_offset = 180;
     for (int64 i = 0; i < frames_n; ++i) {
-        float64 t = py_to_float64((i + start_offset) % frames_n) / py_to_float64(frames_n);
-        float64 angle = 2.0 * pytra::std::math::pi * t;
+        float64 t = py_div(py_to<float64>((i + start_offset) % frames_n), py_to<float64>(frames_n));
+        auto angle = 2.0 * pytra::std::math::pi * t;
         float64 cr = center_cr + radius_cr * pytra::std::math::cos(angle);
         float64 ci = center_ci + radius_ci * pytra::std::math::sin(angle);
         int64 phase = (phase_offset + i * 5) % 255;
-        frames.append(bytes(render_frame(width, height, cr, ci, max_iter, phase)));
+        frames.append(render_frame(width, height, cr, ci, max_iter, phase));
     }
-    pytra::utils::gif::save_gif(out_path, width, height, frames, julia_palette(), int64(py_to_int64(8)), int64(py_to_int64(0)));
+    pytra::utils::gif::save_gif(out_path, width, height, frames, julia_palette(), 8, 0);
     auto elapsed = pytra::std::time::perf_counter() - start;
     py_print("output:", out_path);
     py_print("frames:", frames_n);
