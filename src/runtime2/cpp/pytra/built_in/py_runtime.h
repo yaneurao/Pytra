5d586dcf7cd15e4e18985ad25d826eb9f31a1049
yaneurao <yaneurao@gmail.com>
2026-03-07 09:36:07 +0900

    remove legacy runtime2 tree

--- src/runtime2/cpp/pytra/built_in/py_runtime.h
+++
@@ -1,4 +0,0 @@
-// FORWARDER: handwritten runtime header moved to pytra-core.
-#pragma once
-
-#include "runtime/cpp/pytra-core/built_in/py_runtime.h"
--- src/runtime2/cpp/pytra/std/math-impl.cpp
+++
@@ -1,2 +0,0 @@
-// FORWARDER TU: handwritten runtime source moved to pytra-core.
-#include "runtime/cpp/pytra-core/std/math-impl.cpp"
