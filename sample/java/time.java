afa6883ebac6de88f3068c1dd06728c0162806af
yaneurao <yaneurao@gmail.com>
2026-03-18 21:23:16 +0900

    Java runtime 生成コード追加

---
+++ sample/java/time.java
@@ -0,0 +1,16 @@
+// AUTO-GENERATED FILE. DO NOT EDIT.
+// source: src/pytra/std/time.py
+// generated-by: tools/gen_runtime_from_manifest.py
+
+public final class time {
+    private time() {
+    }
+
+
+    public static double perf_counter() {
+        return time_native.perf_counter();
+    }
+
+    public static void main(String[] args) {
+    }
+}
